7ade33f1344c3ab23c9ffb10c56aa96d90337a7d
Joshua Franklin <franklj1@uci.edu>
2023-04-03 23:01:57 -0700

    Imports stats from the year the user requests into a pandas dataframe for further analysis

--- web-scraper.py
+++ web-scraper.py
@@ -1,9 +1,18 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
+# Gets the year that the user wants to analyze the stats from
+def get_year():
+  year = input('Enter year to collect stats from: ')
+  print('year entered:', year)
+  return int(year)
+
+
+# Extracts the stats from the year into a array
 def extract_stats():
     
-  year = 2022
+  year = get_year()
   url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html".format(year)
   html = urlopen(url)
 
@@ -13,15 +22,21 @@ def extract_stats():
   soup.find_all('tr', limit=2)
   headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
   headers = headers[1:]
-  print(headers)
+  # print(headers)
 
   rows = soup.findAll('tr')[1:]
   player_stats = [[td.getText() for td in rows[i].findAll('td')]
             for i in range(len(rows))]
 
-  print(player_stats[0])
+  # print(player_stats[0])
 
-  
+  create_dataframe(player_stats, headers)
+
+# Uses the array created from extract_stats to put all stats into a pandas dataframe
+def create_dataframe(player_stats, headers):
+  stats = pd.DataFrame(player_stats, columns = headers)
+  print(stats.head(10))
+
 if __name__ == '__main__':
     print('helloworld')
     extract_stats()
