bd30bceb201a55a76dc3fffc29f2fb974ac44482
Joshua Franklin <franklj1@uci.edu>
2023-04-03 23:03:57 -0700

    Removed unnecessary print statements

--- web-scraper.py
+++ web-scraper.py
@@ -17,18 +17,15 @@ def extract_stats():
   html = urlopen(url)
 
   soup = BeautifulSoup(html, 'html.parser')
-  # print(soup)
 
   soup.find_all('tr', limit=2)
   headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
   headers = headers[1:]
-  # print(headers)
 
   rows = soup.findAll('tr')[1:]
   player_stats = [[td.getText() for td in rows[i].findAll('td')]
             for i in range(len(rows))]
 
-  # print(player_stats[0])
 
   create_dataframe(player_stats, headers)
 
@@ -38,5 +35,4 @@ def create_dataframe(player_stats, headers):
   print(stats.head(10))
 
 if __name__ == '__main__':
-    print('helloworld')
     extract_stats()
